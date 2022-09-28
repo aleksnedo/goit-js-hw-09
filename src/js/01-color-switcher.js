fd48546ec512d26611cf4cd609d92c58a02f84ac
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-09-28 16:38:47 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+console.log(startBtn);
+let counter = 0;
+startBtn.addEventListener('click', countDecrement);
+stopBtn.addEventListener('click', countIncrement);
+
+function countDecrement() {
+  counter -= 1;
+}
+function countIncrement() {
+  counter += 1;
+}

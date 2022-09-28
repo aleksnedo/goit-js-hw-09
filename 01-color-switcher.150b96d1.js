dccff1d15cb6d9eddae45a4caf4e51f9739a9e82
aleksnedo <104201289+aleksnedo@users.noreply.github.com>
2022-09-28 13:39:19 +0000

    Deploying to gh-pages from @ aleksnedo/goit-js-hw-09@fd48546ec512d26611cf4cd609d92c58a02f84ac 🚀

---
+++ 01-color-switcher.150b96d1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(t);t.addEventListener("click",(function(){1})),e.addEventListener("click",(function(){1}))}();
+//# sourceMappingURL=01-color-switcher.150b96d1.js.map

9e93af618a57f02faca86e7d2c19cd5250307c2e
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-10-03 17:35:58 +0300

    timer update 1.0

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,28 +1,96 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const TIMER_DEADLINE = new Date(2022, 10, 10, 22, 22);
-let timerId = null;
+const notifyOptions = {
+  position: 'center-center',
+  backOverlay: true,
+  clickToClose: true,
+  closeButton: true,
+};
 
-const timerRef = document.querySelector('.timer');
+const refs = {
+  startBtnRef: document.querySelector('button[data-start]'),
+  inputDateRef: document.querySelector('#datetime-picker'),
+  daysRef: document.querySelector('[data-days]'),
+  hoursRef: document.querySelector('[data-hours]'),
+  minutesRef: document.querySelector('[data-minutes]'),
+  secondsRef: document.querySelector('[data-seconds]'),
+};
 
-const timer = {
-  intervalId: null,
-  refs: {},
-  start(rootSelector, deadline) {
-    const deltaTime = deadline.getTime() - Date.now();
-    console.log(deltaTime);
+refs.startBtnRef.addEventListener('click', () => timer.start());
 
-    if (deltaTime <= 0) {
-      Notify.failure('Please choose a date in the future', {
-        position: 'center-center',
-        backOverlay: true,
-        clickToClose: true,
-        closeButton: true,
-      });
-      return;
+let deadlineTime = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+
+    if (selectedDates[0] <= Date.now()) {
+      // refs.startBtnRef.setAttribute('disabled', false);
+      Notify.failure('Please choose a date in the future', notifyOptions);
+      selectedDates[0] = new Date();
+    } else {
+      refs.startBtnRef.toggleAttribute('disabled');
+      deadlineTime = selectedDates[0].getTime();
     }
   },
 };
 
-timer.start(timerRef, TIMER_DEADLINE);
+class Timer {
+  constructor() {
+    this.intervalId = null;
+    this.isActive = false;
+    refs.startBtnRef.disabled = true;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = new Date();
+      const deltaTime = deadlineTime - currentTime;
+      const timeData = convertMs(deltaTime);
+
+      if (deltaTime <= 1000) {
+        clearInterval(this.intervalId);
+      }
+
+      if (this.isActive) {
+        refs.startBtnRef.disabled = true;
+      }
+
+      refs.daysRef.textContent = timeData.days;
+      refs.hoursRef.textContent = timeData.hours;
+      refs.minutesRef.textContent = timeData.minutes;
+      refs.secondsRef.textContent = timeData.seconds;
+    }, 1000);
+
+    function convertMs(ms) {
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const days = pad(Math.floor(ms / day));
+      const hours = pad(Math.floor((ms % day) / hour));
+      const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+      const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+      return { days, hours, minutes, seconds };
+    }
+
+    function pad(value) {
+      return String(value).padStart(2, '0');
+    }
+  }
+}
+const timer = new Timer();
+flatpickr(refs.inputDateRef, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const NOTIFY_TIMEOUT = 5000; //ms
+
+const promisesFormRef = document.querySelector('.form');
+
+promisesFormRef.addEventListener('submit', onPromisesFormSubmit);
+
+function onPromisesFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formDataObj = {};
+  formData.forEach((value, key) => {
+    formDataObj[key] = Number(value);
+  });
+  // console.log(formDataObj);
+  createPromisesArray(formDataObj);
+}
+
+function createPromisesArray({ delay, step, amount }) {
+  const delaysArray = Array.from(
+    { length: amount },
+    (_, i) => delay + i * step
+  );
+  // console.log(delaysArray);
+  const promises = delaysArray.map(createPromise);
+  // console.log(promises);
+}
+
+function createPromise(delay, position) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`Fulfilled promice ${position + 1} in ${delay} ms`);
+      } else {
+        // Reject
+        reject(`Rejected promice ${position + 1} in ${delay} ms`);
+      }
+    }, delay);
+  })
+    .then(value =>
+      Notify.success(value, {
+        timeout: NOTIFY_TIMEOUT,
+      })
+    )
+    .catch(error =>
+      Notify.failure(error, {
+        timeout: NOTIFY_TIMEOUT,
+      })
+    );
 }
